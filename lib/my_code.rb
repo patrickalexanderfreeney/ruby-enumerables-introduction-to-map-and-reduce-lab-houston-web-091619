660e4be942c016affde2ad4b361cda79df8cba2a
Patrick Alexander <patrick_alexander@mail.com>
2019-09-05 17:51:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,13 +51,13 @@ def reduce_to_total(source_array, starting_point = 0)
   #end
   #return new_array
   
-    new = starting_point
+    new_array = starting_point
   index = 0
   while index < source_array.length do
     new += source_array[index]
     index += 1
   end
-  return new
+  return new_array
 
 end
 
